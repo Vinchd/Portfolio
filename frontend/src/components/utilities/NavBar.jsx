e6f4d9f6b1fd71bf028885203d9d13c57558b097
Vinch <vince.daviaud@gmail.com>
2023-07-20 01:07:59 +0200

    update nav graph

--- frontend/src/components/utilities/NavBar.jsx
+++ frontend/src/components/utilities/NavBar.jsx
@@ -1,12 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { Gitgraph, templateExtend, TemplateName } from "@gitgraph/react";
 
 export default function NavBar() {
+  const navigate = useNavigate();
   const withoutHash = templateExtend(TemplateName.Metro, {
-    // branch: {
-    //   label: {
-    //     display: false,
-    //   },
-    // },
+    branch: {
+      label: {
+        display: false,
+      },
+    },
     commit: {
       message: {
         displayHash: false,
@@ -18,42 +20,80 @@ export default function NavBar() {
   const options = {
     author: " ",
     template: withoutHash,
-    mode: "compact",
   };
 
-  const handleClick = () => {
-    // Redirige vers la route /pages/Home
-    window.location.replace("/pages/NotFound");
+  const handleClick = (page) => {
+    navigate(`/${page}`);
   };
 
   return (
-    <nav className="flex max-w-[320px] flex-[1] flex-col">
-      {/* <div>Accueil</div>
-      <div>Mon CV</div>
-      <div>Contact</div> */}
-      <Gitgraph options={options}>
+    <nav className="nav flex h-full max-w-[320px] flex-[1] flex-col items-center justify-center bg-tertiary">
+      {/* <Gitgraph options={options} className="hidden">
         {(gitgraph) => {
-          const mainBranch = gitgraph.branch({
-            name: <a href="https://www.google.fr/">Accueil</a>,
-          });
-          mainBranch.commit();
-          const devBranch = gitgraph.branch({
-            name: <a href="https://www.google.fr/">Mon CV</a>,
-          });
+          const mainBranch = gitgraph.branch("main");
+          mainBranch.commit("");
+          const devBranch = gitgraph.branch("dev");
 
-          devBranch.commit({
-            subject: "test23",
+          devBranch.commit("");
+          const featureBranch = gitgraph.branch("feature");
+          featureBranch.commit({
+            subject: "Contact",
             onMessageClick() {
-              handleClick();
+              handleClick("contact");
             },
           });
-          const featureBranch = gitgraph.branch({
-            name: <a href="https://www.google.fr/">Contact</a>,
+          devBranch.commit("");
+          featureBranch.commit({
+            subject: "Mon CV",
+            onMessageClick() {
+              handleClick("cv");
+            },
           });
-          featureBranch.commit().commit();
-          devBranch.merge(featureBranch);
+          devBranch.commit("");
+          featureBranch.commit({
+            subject: "Accueil",
+            onMessageClick() {
+              handleClick("");
+            },
+          });
+          devBranch.merge(featureBranch, " ");
 
-          mainBranch.merge(devBranch);
+          mainBranch.merge(devBranch, " ");
+        }}
+      </Gitgraph> */}
+      <Gitgraph options={options} className="hidden">
+        {(gitgraph) => {
+          const mainBranch = gitgraph.branch("main");
+          mainBranch.commit("");
+          const devBranch = gitgraph.branch("dev");
+
+          devBranch.commit("");
+          const featureBranch = gitgraph.branch("feature");
+          featureBranch.commit({
+            subject: "Contact",
+            onMessageClick() {
+              handleClick("contact");
+            },
+          });
+          devBranch.merge(featureBranch, " ");
+
+          featureBranch.commit({
+            subject: "Mon CV",
+            onMessageClick() {
+              handleClick("cv");
+            },
+          });
+          devBranch.merge(featureBranch, " ");
+
+          featureBranch.commit({
+            subject: "Accueil",
+            onMessageClick() {
+              handleClick("");
+            },
+          });
+          devBranch.merge(featureBranch, " ");
+
+          mainBranch.merge(devBranch, " ");
         }}
       </Gitgraph>
     </nav>
