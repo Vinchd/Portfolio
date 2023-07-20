fafde7acf430510d59c54b656b9da5aa1a76d039
Vinch <vince.daviaud@gmail.com>
2023-07-21 00:23:51 +0200

    link in img

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import { NavLink } from "react-router-dom";
 import GitHubCalendar from "react-github-calendar";
 
-import Footer from "../components/utilities/Footer";
-
 export default function Home() {
   return (
     <>
       <section className="flex w-full flex-[1] flex-col items-center justify-center lg:min-h-[calc(100dvh-136px)]">
+        <img
+          // src={`${import.meta.env.VITE_BACKEND_URL}/assets/wines/${
+          //   e.wineImage
+          // }`}
+          src={`${
+            import.meta.env.VITE_BACKEND_URL
+          }/public/uploads/thumbnail_ravenfeed.png`}
+          alt="miniature"
+          className="h-48 w-48 rounded"
+        />
         <div className="justify-center">toto</div>
         <GitHubCalendar username="vinchd" fontSize={8} />
       </section>
