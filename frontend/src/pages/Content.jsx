ff48f388e66db0fbbc02b68d85f25d0a4c9e7082
Vinch <vince.daviaud@gmail.com>
2023-07-31 00:54:43 +0200

    create 3d carousel

--- frontend/src/pages/Content.jsx
+++ frontend/src/pages/Content.jsx
@@ -1,12 +1,14 @@
 import Home from "../components/contents/Home";
 import Contact from "../components/contents/Contact";
 import Cv from "../components/contents/Cv.jsx";
+import Projects from "../components/contents/Projects.jsx";
 
 export default function Content() {
   return (
     <>
       <Home />
-      {/*<Projects />*/}
+      <Projects />
+      {/*<ProjectSwiper />*/}
       <Cv />
       <Contact />
     </>
