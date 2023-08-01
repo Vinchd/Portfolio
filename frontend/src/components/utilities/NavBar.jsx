16d17653a8e3ad7d1dfc6b2e7475bb5f29862ada
Vinch <vince.daviaud@gmail.com>
2023-08-01 18:48:30 +0200

    set default height on NavBar.jsx

--- frontend/src/components/utilities/NavBar.jsx
+++ frontend/src/components/utilities/NavBar.jsx
@@ -6,10 +6,39 @@ import NavHome, {
   NavProjects,
 } from "./datanavbar/DataNavBar.jsx";
 
+let height;
+switch (true) {
+  case window.innerHeight >= 1480:
+    height = 150;
+    break;
+  case window.innerHeight >= 1300 && window.innerHeight < 1480:
+    height = 130;
+    break;
+  case window.innerHeight >= 1140 && window.innerHeight < 1300:
+    height = 112;
+    break;
+  case window.innerHeight >= 990 && window.innerHeight < 1140:
+    height = 95;
+    break;
+  case window.innerHeight >= 860 && window.innerHeight < 990:
+    height = 80;
+    break;
+  case window.innerHeight >= 760 && window.innerHeight < 860:
+    height = 70;
+    break;
+  case window.innerHeight >= 670 && window.innerHeight < 760:
+    height = 60;
+    break;
+  case window.innerHeight >= 580 && window.innerHeight < 670:
+    height = 50;
+    break;
+  default:
+    height = 44;
+}
 export default function NavBar({ activeSection, center }) {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
-  const [value, setValue] = useState(81);
+  const [value, setValue] = useState(height);
   const [graphKey, setGraphKey] = useState(0);
 
   const withoutHash = templateExtend(TemplateName.Metro, {
@@ -24,11 +53,11 @@ export default function NavBar({ activeSection, center }) {
         displayHash: false,
         displayAuthor: false,
         font: "normal 14pt Poppins",
-        color: "#E6EDF3",
+        // color: "#E6EDF3",
       },
       spacing: value,
     },
-    colors: ["#415575", "#238636", "#7699CF", "#565554", "#D81159"],
+    colors: ["#415575", "#8EB9FA", "#DAE7FB", "#565554", "#D81159"],
   });
   const options = {
     author: " ",
@@ -49,6 +78,26 @@ export default function NavBar({ activeSection, center }) {
       setWindowWidth(window.innerWidth);
       const newHeight = window.innerHeight;
       setWindowHeight(newHeight);
+      if (newHeight > 1480) {
+        setGraphKey((prevKey) => prevKey + 1);
+        setValue(150);
+      }
+      if (newHeight > 1460 && newHeight < 1480) {
+        setGraphKey((prevKey) => prevKey + 1);
+        setValue(130);
+      }
+      if (newHeight > 1280 && newHeight < 1300) {
+        setGraphKey((prevKey) => prevKey + 1);
+        setValue(112);
+      }
+      if (newHeight > 1120 && newHeight < 1140) {
+        setGraphKey((prevKey) => prevKey + 1);
+        setValue(95);
+      }
+      if (newHeight > 970 && newHeight < 990) {
+        setGraphKey((prevKey) => prevKey + 1);
+        setValue(80);
+      }
       if (newHeight > 840 && newHeight < 860) {
         setGraphKey((prevKey) => prevKey + 1);
         setValue(70);
