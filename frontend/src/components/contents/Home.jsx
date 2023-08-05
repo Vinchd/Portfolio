7c1d7f2cfa442749d92f8dddcb89114cf5284ab5
Vinch <vince.daviaud@gmail.com>
2023-08-05 18:43:35 +0200

    import react-parallax-tilt in Home.jsx and create 2 tilts with paragraph

--- frontend/src/components/contents/Home.jsx
+++ frontend/src/components/contents/Home.jsx
@@ -1,4 +1,5 @@
 import GitHubCalendar from "react-github-calendar";
+import Tilt from "react-parallax-tilt";
 import NavBar from "../utilities/NavBar.jsx";
 
 export default function Home() {
@@ -6,13 +7,48 @@ export default function Home() {
     <div className="flex">
       <NavBar activeSection="home" />
       <section id="home" className="scroll-area">
-        <div className="justify-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sed,
-          fugiat placeat quae reprehenderit quisquam nulla vitae, nemo quod
-          expedita assumenda eveniet odio suscipit corrupti. Laboriosam maiores
-          veritatis cum id illo dicta nostrum molestiae temporibus optio, ab
-          expedita suscipit dolore autem excepturi consequatur deserunt
-          necessitatibus nobis corrupti incidunt. At, distinctio!
+        {/*Card droite text presentation. Card gauche parcours/compétences avec nombre infini*/}
+        <div>
+          <div className="flex items-center">
+            <p className="w-2/3">
+              Bienvenue sur mon Portfolio, je m'appel Vincent et je suis
+              développeur Web junior à la recherche d'une alternance d'une durée
+              d'un an.
+            </p>
+            <Tilt
+              className="parallax-effect-glare-scale"
+              perspective={500}
+              glareEnable={false}
+              glareMaxOpacity={0.45}
+              gyroscope={true}
+            >
+              <div className="inner-element">
+                <div>React</div>
+                <div>Parallax Tilt</div>
+                <div>👀</div>
+              </div>
+            </Tilt>
+          </div>
+          <div className="flex translate-y-[-50px] flex-row-reverse items-center">
+            <p className="w-2/3">
+              Bienvenue sur mon Portfolio, je m'appel Vincent et je suis
+              développeur Web junior à la recherche d'une alternance d'une durée
+              d'un an.
+            </p>
+            <Tilt
+              className="parallax-effect-glare-scale"
+              perspective={500}
+              glareEnable={false}
+              glareMaxOpacity={0.45}
+              gyroscope={true}
+            >
+              <div className="inner-element">
+                <div>React</div>
+                <div>Parallax Tilt</div>
+                <div>👀</div>
+              </div>
+            </Tilt>
+          </div>
         </div>
         <GitHubCalendar username="vinchd" fontSize={8} />
       </section>
